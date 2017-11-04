30887126a95dd75ba06adadc1b76501d38eff43e
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-11-04 21:41:29 +0300

    Register Blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -35,5 +35,10 @@ def create_app(config_option):
         db_adapter, app, register_form=MyRegisterForm, user_profile_view_function=user_profile_page)
 
     # Register Blueprints
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/authenticate')
 
     return app
