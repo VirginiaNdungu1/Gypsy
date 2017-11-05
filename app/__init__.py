f4ac31f0eea9f7f63b61c0fab799285cc69c1185
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-11-05 14:50:01 +0300

    Update - add LoginManager Class
    Initialise it

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,17 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from config import config_options
-from flask_babelex import Babel
-from flask_user import UserManager, UserMixin, SQLAlchemyAdapter
+
+from flask_login import LoginManager
+
 
 # initialise Flask extensions
 bootstrap = Bootstrap()
-babel = Babel()
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_message = "You must be logged in to access this page."
+login_manager.login_view = 'auth.login'
 
 
 def create_app(config_option):
@@ -21,18 +25,7 @@ def create_app(config_option):
     # initialise flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
-    babel.init_app(app)
-
-    # Setup flask_user to handle user account related forms
-    from .models.user_models import User, MyRegisterForm
-    from .views.misc_views import user_profile_page
-
-    # Setup the SQLAlchemy DB Adapter
-    db_adapter = SQLAlchemyAdapter(db, User)
-
-    # Initialise Flask-User and bind to app
-    user_manager = UserManager(
-        db_adapter, app, register_form=MyRegisterForm, user_profile_view_function=user_profile_page)
+    login_manager.init_app(app)
 
     # Register Blueprints
     from .main import main as main_blueprint
