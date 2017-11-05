7363190ab62a798785ecabc3510ce4d6fff0f866
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-11-05 17:09:30 +0300

    Create and register admin blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -28,10 +28,14 @@ def create_app(config_option):
     login_manager.init_app(app)
 
     # Register Blueprints
+
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/authenticate')
+
+    from .admin import admin as admin_blueprint
+    app.register_blueprint(admin_blueprint, url_prefix='/admin')
 
     return app
